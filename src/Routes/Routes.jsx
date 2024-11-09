6f26510280e2cbc5cfdff1840144676ad9e90096
MukitHossen7 <hossenmukit7@gmail.com>
2024-11-09 19:26:51 +0600

    header component done

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayouts from "../Layout/MainLayouts";
 import Home from "../Pages/Home/Home";
+import LogIn from "../components/LogIn/LogIn";
+import Register from "../components/Register/Register";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +12,14 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <LogIn></LogIn>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
       },
     ],
   },
